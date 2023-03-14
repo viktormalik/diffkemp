2a7bb4c11809c30945626c57dbb24066a78aa899
Daniel Kříž <Daniel.kriz@protonmail.com>
2023-03-14 23:13:18 +0100

    PatternGenerator: init

---
+++ diffkemp/simpll/PatternGenerator.cpp
@@ -0,0 +1,60 @@
+#include "PatternGenerator.h"
+
+void PatternGenerator::addFileForInference(std::string fileName) const {
+    llvm::LLVMContext ctx;
+    llvm::SMDiagnostic err;
+    std::unique_ptr<llvm::Module> mod(llvm::parseIRFile(fileName, err, ctx));
+    if (!mod) {
+        err.print("Diffkemp", llvm::errs());
+    }
+    for (auto &func : *mod) {
+        /// If patter is null, then this is the first call of the function,
+        /// in that case copy the function from first module and use it in
+        /// further generation.
+        if (isFreshRun) {
+            pattern = std::make_unique<llvm::Module>("tmp", ctx);
+
+            auto patternFunc = llvm::Function::Create(func.getFunctionType(),
+                                                      func.getLinkage(),
+                                                      "tmp_add",
+                                                      *pattern);
+            if (patternFunc == nullptr) {
+                std::cerr << "Error: Pattern func is null" << std::endl;
+                return;
+            }
+
+            llvm::ValueToValueMapTy tmp_vmap;
+
+            auto patternFuncArgIter = patternFunc->arg_begin();
+            for (auto &arg : func.args()) {
+                patternFuncArgIter->setName(arg.getName());
+                tmp_vmap[&arg] = &(*patternFuncArgIter++);
+            }
+
+            llvm::SmallVector<llvm::ReturnInst *, 8> returns;
+            llvm::CloneFunctionInto(
+                    patternFunc,
+                    &func,
+                    tmp_vmap,
+                    llvm::CloneFunctionChangeType::DifferentModule,
+                    returns);
+
+            for (auto &BB : *patternFunc) {
+                for (auto &inst : BB) {
+                    /// Attach some kind of metadata in case of `store`
+                    /// instruction
+                    if (inst.getOpcodeName() == std::string("store")) {
+                        auto &instrCtx = inst.getContext();
+                        llvm::MDNode *node = llvm::MDNode::get(
+                                instrCtx,
+                                llvm::MDString::get(instrCtx,
+                                                    "Diffkemp Patter"));
+                        inst.setMetadata("diffkemp.pattern", node);
+                    }
+                }
+            }
+            pattern->dump();
+            isFreshRun = false;
+        }
+    }
+}
---
+++ diffkemp/simpll/PatternGenerator.h
@@ -0,0 +1,49 @@
+#ifndef DIFFKEMP_SIMPLL_PATTERNGENERATOR_H
+#define DIFFKEMP_SIMPLL_PATTERNGENERATOR_H
+
+#include <llvm/ADT/SmallVector.h>
+#include <llvm/Analysis/MemorySSAUpdater.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Metadata.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/Transforms/Utils/Cloning.h>
+
+#include <iostream>
+
+class PatternGenerator {
+  public:
+    PatternGenerator() : isFreshRun(true) {
+    };
+
+    virtual ~PatternGenerator() = default;
+
+    PatternGenerator(const PatternGenerator &other) noexcept {
+        isFreshRun = other.isFreshRun;
+    };
+
+    PatternGenerator(PatternGenerator &&other) noexcept {
+        std::swap(isFreshRun, other.isFreshRun);
+    };
+
+    PatternGenerator &operator=(const PatternGenerator &other) {
+        return *this = PatternGenerator(other);
+    }
+
+    PatternGenerator &operator=(PatternGenerator &&other) {
+        std::swap(isFreshRun, other.isFreshRun);
+        return *this;
+    }
+
+    void addFileForInference(std::string fileName) const;
+
+  private:
+    mutable bool isFreshRun;
+    mutable std::unique_ptr<llvm::Module> pattern;
+};
+
+#endif // DIFFKEMP_SIMPLL_PATTERNGENERATOR_H
