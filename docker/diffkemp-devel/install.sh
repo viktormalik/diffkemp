6ddacc4c289ba4e914c3f846efae28323acef655
Daniel Kříž <Daniel.kriz@protonmail.com>
2023-03-14 23:07:22 +0100

    docker: manually symlink pip in install script
    For some unknown reason pip cannot be found in the PATH and because of
    that automatic installation fails everytime.

--- docker/diffkemp-devel/install.sh
+++ docker/diffkemp-devel/install.sh
@@ -5,4 +5,6 @@ cd $SIMPLL_BUILD_DIR
 cmake $DIFFKEMP_DIR -GNinja -DCMAKE_BUILD_TYPE=Debug
 ninja -j4
 cd -
+ln -sr $(which pip3) /usr/bin/pip
+ln -sr $(which pip3) /usr/bin/pip3
 python3 -m pip install -e $DIFFKEMP_DIR
